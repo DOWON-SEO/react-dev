bb0c04ae7fe3f48f5dfdc3b2f025d6aad884a550
TheLifeOfSeo <dsa3182@gmail.com>
2023-04-16 22:54:32 +0900

    3-1. 아이콘, CSS 라이브러리 사용
    Google Material Icon 패키지 설치
    Tailwind CSS의 간단한 문법

---
+++ ch03/ch03_1/src/App.tsx
@@ -0,0 +1,15 @@
+import BootStrap from "./pages/Bootstrap";
+import Icon from "./pages/Icon";
+import UsingIcon from "./pages/UsingIcon";
+
+function App() {
+    return (
+        <div>
+            <UsingIcon/>
+            <Icon/>
+            <BootStrap/>
+        </div>
+    );
+}
+
+export default App;
---
+++ ch03/ch03_1/src/pages/Icon.tsx
@@ -0,0 +1,10 @@
+export default function Icon() {
+    return(
+        <div>
+            <h3>Icon</h3>
+            <div className="material-icons">home</div>
+            <div className="material-icons">check_circle_outline</div>
+            
+        </div>
+    )
+}
